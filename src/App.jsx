9ccb5981a9e77fdc960011768622232771a996ba
Aristo1989 <cristianalejo2407@gmail.com>
2025-05-08 22:19:45 -0500

    primer Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import "./App.css";
+import Login from "./components/Login/login";
 
 function App() {
-  
-
- 
-
   return (
-    <div className="h-screen w-screen bg-blue-400 flex justify-center items-center">
-      Hola
+    <div>
+      <Login />
     </div>
   );
 }
